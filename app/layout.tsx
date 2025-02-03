95a95e605ae1df51ca809c2a43abc18ddf5924b5
arpynond <ynon.d@arpeely.com>
2025-02-03 11:35:10 +0200

    Update from V0

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,8 @@
 import { Layout } from "@/components/layout"
 import { ClientInit } from "@/components/client-init"
+import { AttributeCleaner } from "@/components/attribute-cleaner"
 import "@/styles/globals.css"
-import type React from "react" // Added import for React
+import type React from "react"
 
 export const metadata = {
   title: "RP Ads Manager",
@@ -16,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <AttributeCleaner />
         <ClientInit />
         <Layout>{children}</Layout>
       </body>
--- components/layout.tsx
+++ components/layout.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import Link from "next/link"
 import { BarChart3, Settings, Square, SquareStack } from "lucide-react"
 import { cn } from "@/lib/utils"
